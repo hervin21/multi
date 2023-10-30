a7eb1226164b612af8b8c16626a745290e8eec3f
hervin21 <142458280+hervin21@users.noreply.github.com>
2023-10-29 20:15:37 -0600

    Agregar archivos de proyecto.

---
+++ multi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TablasDeMultiplicarConWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, T, I, RESUL;
+            string linea;
+
+            Console.Write("CUANTAS TABLAS: ");
+            linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+
+            T = 1;
+
+            while (T <= NUM)
+            {
+                I = 10;
+
+                while (I >= 1)
+                {
+                    RESUL = T * I;
+                    Console.WriteLine("{0} x {1} = {2}", T, I, RESUL);
+                    I = I - 1;
+                }
+
+                T = T + 1;
+            }
+
+            Console.Write("Pulse una Tecla:");
+            Console.ReadLine();
+        }
+    }
+}
